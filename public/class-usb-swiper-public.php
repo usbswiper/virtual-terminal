7dfe7cca4689939e0f59cfaf79a8bf2073b5b25c
Drew Angell <drew@angelleye.com>
2024-10-11 16:10:06 -0500

    Adds description to invoice prefix setting, VT-128

--- public/class-usb-swiper-public.php
+++ public/class-usb-swiper-public.php
@@ -2232,7 +2232,8 @@ if( !class_exists( 'Usb_Swiper_Public' ) ) {
                     'name' => 'InvoicePrefix',
                     'label' => __( 'Invoice Prefix', 'usb-swiper'),
                     'attributes' => '',
-                    'description' => '',
+                    'description' => 'Enter a unique value here to avoid duplicate invoice ID errors from PayPal.  For example, if your 
+                    company name is Acme, Inc. you might use ACME- as your prefix.',
                     'readonly' => false,
                     'disabled' => false,
                     'class' => 'woocommerce-Input woocommerce-Input--text input-text',
