0f384ab5751a3dbfcbdf8bc6e4473e60415c8b43
Nirmal Desai <nirmal.desai@creedally.com>
2024-10-11 18:14:00 +0530

    Review changes conflict resolved

--- assets/js/usb-swiper.js
+++ assets/js/usb-swiper.js
@@ -1294,7 +1294,7 @@ jQuery( document ).ready(function( $ ) {
                     checkbox.after(reviewChangesButton);
                 } else if('' === changedFields){
                     $('.review-changes').remove();
-                    $('.user-review-list').remove();
+                    $('.reviewChangesList').remove();
                 }
 
             }
