c6a20aca4c1dd7f59f7437f249545ac56d1b0765
Priyank Vadhvana <priyank.vadhvana@creedally.com>
2024-10-18 21:21:53 +0530

    Removed merge conflict issue

--- templates/virtual-terminal-form.php
+++ templates/virtual-terminal-form.php
@@ -18,24 +18,14 @@ if( true === $profile_status && !empty($merchant_id)) {
         ];
     }
 
-    if( ! usbswiper_get_invoice_prefix() ){
+    if( ! usbswiper_get_invoice_prefix() ) {
         $disable_payment = 'disabled';
-        $edit_page =  wc_get_account_endpoint_url( 'edit-account' );
+        $edit_page = wc_get_account_endpoint_url('edit-account');
         $notifications[] = [
             'type' => 'error',
-            'message' => sprintf(__('Please add an Invoice Prefix under your %s.  <br />If you have questions about this or would like some help, please call 224-677-0283 x4.', 'usb-swiper'), '<a href="'.esc_url($edit_page).'">'.__('Account Details Settings', 'usb-swiper').'</a>')
+            'message' => sprintf(__('Please add an Invoice Prefix under your %s.  <br />If you have questions about this or would like some help, please call 224-677-0283 x4.', 'usb-swiper'), '<a href="' . esc_url($edit_page) . '">' . __('Account Details Settings', 'usb-swiper') . '</a>')
         ];
     }
-
-    if( ! usbswiper_get_invoice_prefix() ){
-        $disable_payment = 'disabled';
-        $edit_page =  wc_get_account_endpoint_url( 'edit-account' );
-        $notifications[] = [
-            'type' => 'error',
-            'message' => sprintf(__('Kindly add the Invoice Prefix on %s to initiate the transaction.', 'usb-swiper'), '<a href="'.esc_url($edit_page).'">'.__('My account', 'usb-swiper').'</a>')
-        ];
-    }
-
 ?>
 <div class="vt-form-wrap woocommerce">
     <div class="vt-form-notification">
